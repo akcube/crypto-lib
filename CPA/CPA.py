d7bb8dfaaa5e6d2977759193f36d95122284e3eb
akcube <a.kishorekumar@students.iiit.ac.in>
2023-02-22 17:17:34 +0530

    CPA decryption, cleanup

--- CPA/CPA.py
+++ CPA/CPA.py
@@ -1,5 +1,5 @@
 from PRF.PRF import PRF
-from util.util import xor, binstr
+from util.util import xor, binstr, blockify
 
 
 class CPA:
@@ -9,7 +9,7 @@ class CPA:
                  prime_field: int,
                  generator: int,
                  key: int,
-                 mode="CTR"):
+                 mode="CTR") -> None:
         """
         Initialize the values here
         :param security_parameter: 1ⁿ
@@ -28,46 +28,53 @@ class CPA:
         """
         if (mode not in ['CTR', 'OFB', 'CBC']):
             raise ValueError(f"Unsupported mode: {self.mode}")
-
         self.n = security_parameter
         self.prf = PRF(security_parameter, generator, prime_field, key)
         self.mode = mode
 
-    def enc(self, message: str, random_seed: int) -> str:
+        f = lambda rs: binstr(self.prf.evaluate(rs), self.n)
+        self.ops = {
+            'CTR': lambda b, rs: (xor(b, f(rs + 1)), rs + 1),
+            'OFB': lambda b, rs: (xor(b, f(rs)), int(f(rs), 2)),
+            'CBC': lambda b, rs:
+            (xor(f(int(b, 2) ^ rs), b), int(f(int(b, 2) ^ rs), 2))
+        }
+
+    def crypt(self, message: str, random_seed: int) -> str:
         """
-        Encrypt message against Chosen Plaintext Attack using randomized ctr mode
+        Applies the appropriate encryption operation to the message string
         :param message: m
         :type message: int
-        :param random_seed: ctr
+        :param random_seed: r
         :type random_seed: int
+        :return: Applies correct encryption operation to message string
+        :rtype: str
         """
-
-        blockify = lambda s, block_len: [
-            s[i:i + block_len] for i in range(0, len(s), block_len)
-        ]
-        secret = lambda rs: binstr(self.prf.evaluate(rs), self.n)
-
-        message = blockify(message, self.n)
-        cipher = binstr(random_seed, self.n)
-        for b in message:
-            if self.mode == 'CTR':
-                random_seed += 1
-                cipher += xor(b, secret(random_seed))
-            elif self.mode == 'OFB':
-                sec = secret(random_seed)
-                cipher += xor(b, sec)
-                random_seed = int(sec, 2)
-            elif self.mode == 'CBC':
-                sec = secret(int(b, 2) ^ random_seed)
-                cipher += sec
-                random_seed = int(sec, 2)
+        cipher, op = "", self.ops[self.mode]
+        for b in blockify(message, self.n):
+            cipher_block, random_seed = op(b, random_seed)
+            cipher += cipher_block
         return cipher
+
+    def enc(self, message: str, random_seed: int) -> str:
+        """
+        Encrypt message against Chosen Plaintext Attack via initialized mode
+        :param message: m
+        :type message: int
+        :param random_seed: r
+        :type random_seed: int
+        :return: Encoded cipher
+        :rtype: str
+        """
+        return binstr(random_seed, self.n) + self.crypt(message, random_seed)
 
     def dec(self, cipher: str) -> str:
         """
         Decrypt ciphertext to obtain plaintext message
         :param cipher: ciphertext c
         :type cipher: str
+        :return: Decoded message
+        :rtype: str
         """
-        cipher, rseed = cipher[self.n:], int(cipher[:self.n], 2)
-        
+        cipher, random_seed = cipher[self.n:], int(cipher[:self.n], 2)
+        return self.crypt(cipher, random_seed)
--- util/util.py
+++ util/util.py
@@ -22,3 +22,16 @@ def xor(x: str, y: str) -> str:
     :rtype: str
     """
     return binstr(int(x, 2) ^ int(y, 2), max(len(x), len(y)))
+
+
+def blockify(s: str, block_len: int) -> list[str]:
+    """
+    Slices s into a list of consecutive subarrays of length block_len. If len(s) % block_len != 0 the last element will not be padded.
+    :param s: string to be sliced
+    :type s: str
+    :param block_len: length of the slice
+    :type block_len: int
+    :return: List containing the sliced strings
+    :rtype: list[str]
+    """
+    return [s[i:i + block_len] for i in range(0, len(s), block_len)]
